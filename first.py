d7c2aa4b20411301702a48c1791653ac1ba201cf
lalit97 <sutharlalit.97@gmail.com>
2020-03-19 17:00:29 +0530

    initial commit

---
+++ first.py
@@ -0,0 +1,104 @@
+
+# single line comment 
+# printing hello world 
+
+
+#multiple line comment 
+'''
+asdlfjk
+asdfkljadf
+asdfljksdaf
+'''
+
+
+#print functions 
+# print("Hello World!")
+
+#maths operator 
+# print(10+15)
+
+
+# variables printing
+
+cars = 100
+#name = "Nirmala Sainsara"
+
+# print(cars)
+# print(name)
+
+
+# variables plus messages 
+
+# print(name, "has total", cars, "number of cars")
+
+# print("{print_name} has total {print_cars} number of cars".format(
+# 	print_name=name, print_cars=cars))
+
+# print(f"{name} has total {cars} number of cars")
+
+
+# int x = 5;
+# int y = 6;
+# printf("%d %d", x, y);
+
+
+'''
+Taking inputs
+'''
+#print("enter your name")
+#name = input()
+#print("enter your age")
+#age = int(input())
+#print(f"{name}'s age is {age}")
+
+#print(type(age))
+
+####
+
+# def print_args(age, name):
+# 	if age > 22:
+# 		print("age is greater than 22")
+# 	elif age > 20:
+# 		print("somewhere between 20 and 22")
+# 	else:
+# 		print("age is less than 22")
+# 	#print(f"{name}'s age is {age}")
+
+
+# if __name__ == '__main__':
+# 	name = 'ram'
+# 	age = 21
+# 	print_args(age, name)
+
+
+# age_list = [23, 45, 66]
+# for age in age_list:
+# 	print(age, end=' ')
+# print(2)
+
+'''
+range function
+'''
+# numbers = []
+
+# for i in range(5):
+# 	print(i)
+
+###################
+
+# numbers = []
+# print("taking inputs")
+# for i in range(3):
+# 	num = int(input())
+# 	numbers.append(num)
+
+
+# print("printing numbers...")
+# print(numbers)
+# for num in numbers:
+# 	print(num)
+
+##############
+
+numbers = list(map(int, input().split()))
+print(numbers)
---
+++ missing_number.py
@@ -0,0 +1,47 @@
+
+
+# search numbers from 1 to n
+# first missing will be our answer
+
+'''
+searching
+  liniar n
+  binary logn + sorting
+
+  compexity -> nlogn
+
+
+for i in range(1, 10):
+	search(i)
+	if not found:
+		print()
+'''
+#######
+'''
+for i in range(1, 10):
+    i == lis[i-1]
+
+ complexity -> n + nlogn
+'''
+########
+'''
+n
+sum(1-10) - sum(input)
+'''
+
+def missing_number(lis, n):
+	lis = sorted(lis)
+	for i in range(1, n):
+		if i != lis[i-1]:
+			return i
+	return i+1
+
+
+if __name__ == '__main__':
+	t = int(input())
+	for i in range(t):
+		n = int(input())
+		lis = list(map(int, input().split()))
+		ans = missing_number(lis, n)
+		print(ans)
+
