fa714ad39855a8d28aa3c2dcb49c84767d135ab3
lalit97 <sutharlalit.97@gmail.com>
2020-05-08 13:32:49 +0530

    updated answers

--- day3-first-non-repeating.py
+++ day3-first-non-repeating.py
@@ -3,8 +3,13 @@
 https://practice.geeksforgeeks.org/problems/non-repeating-character/0
 
 (2) Example
-
+    string: lhello
+    answe: h
 (3) Idea
+    1. count characters
+    2. and then loop through the string
+    and print the first charcter which has 
+    count 1
 """
 
 """
@@ -12,10 +17,23 @@ complete the function non_repeat()
 return the first non repeating character
 """
 
+def get_counter(string):
+    counter = {}
+    for char in string:
+        if char not in counter:
+            counter[char] = 1
+        else:
+            counter[char] += 1
+    return counter
 
 
 def non_repeat(string, n):
-	pass
+    counter = get_counter(string)
+    for char in string:
+        if counter[char] == 1:
+            return char
+    return -1
+
 
 if __name__ == '__main__':
     for _ in range(int(input())):
--- day3-min-max-in-array.py
+++ day3-min-max-in-array.py
@@ -2,9 +2,18 @@
 (1) Problem
 https://practice.geeksforgeeks.org/problems/find-minimum-and-maximum-element-in-an-array/0
 
+Constraints:
+1 <= T <= 100
+1 <= N <= 1000
+1 <= A[i] <= 10^12
+
 (2) Example
+	arr = [1,3,5,6,13]
+	ans = min = 1, max = 13
 
 (3) Idea 
+	just iterate through loop 
+	and get min and max
 """
 
 """
@@ -13,7 +22,15 @@ return minimum and maximum from array
 """
 
 def minmax(arr, n):
-	pass
+	max = 0
+	min = 10**12
+	for item in arr:
+		if item > max:
+			max = item
+		if item < min:
+			min = item
+	return (min, max)
+
 
 if __name__ == '__main__':
 	for _ in range(int(input())):
