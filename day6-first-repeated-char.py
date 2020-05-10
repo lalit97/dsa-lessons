49007b40959772083fa752aa968d307d8fcd4756
lalit97 <sutharlalit.97@gmail.com>
2020-05-11 00:16:29 +0530

    new problems

---
+++ day6-first-repeated-char.py
@@ -0,0 +1,13 @@
+"""
+https://practice.geeksforgeeks.org/problems/find-first-repeated-character/0
+"""
+
+
+def first_repeat(string):
+	pass
+
+if __name__ == '__main__':
+    for _ in range(int(input())):
+        string = input()
+        ans = first_repeat(string)
+        print(ans)
---
+++ day6-urlify-string.py
@@ -0,0 +1,16 @@
+"""
+https://practice.geeksforgeeks.org/problems/urlify-a-given-string/0
+"""
+
+
+
+
+def urlify(string, k):
+	pass
+
+
+if __name__ == '__main__':
+	for _ in range(int(input())):
+		string = input()
+		k = input()
+		ans = urlify(string, k)
