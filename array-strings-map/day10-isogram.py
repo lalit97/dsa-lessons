479af60ba664232e70621af2fac656c336a6b5e7
lalit97 <sutharlalit.97@gmail.com>
2020-05-17 00:42:15 +0530

    new problems

---
+++ array-strings-map/day10-isogram.py
@@ -0,0 +1,16 @@
+"""
+https://practice.geeksforgeeks.org/problems/check-if-a-string-is-isogram-or-not-1587115620/1
+"""
+
+
+
+
+def is_isogram(string):
+	pass
+
+
+if __name__ == '__main__':
+	for _ in range(int(input())):
+		string = input()
+		ans = is_isogram(string)
+		print(ans)
---
+++ array-strings-map/day10-twice-counter.py
@@ -0,0 +1,17 @@
+"""
+https://practice.geeksforgeeks.org/problems/twice-counter/0
+"""
+
+
+
+
+def twice_counter(string, length):
+	pass
+
+
+if __name__ == '__main__':
+	for _ in range(int(input())):
+		length = int(input())
+		string = input()
+		ans = twice_counter(string, length)
+		print(ans)
