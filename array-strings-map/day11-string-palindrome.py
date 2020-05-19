acb5d110d6a1dc8b14d81f9c816c800feb3563d1
lalit97 <sutharlalit.97@gmail.com>
2020-05-19 18:27:59 +0530

    new problems

---
+++ array-strings-map/day11-string-palindrome.py
@@ -0,0 +1,16 @@
+"""
+https://practice.geeksforgeeks.org/problems/palindrome-string/0
+"""
+
+"""
+return `Yes` or `No`
+"""
+def is_palindrome(string, length):
+    pass
+
+if __name__ == '__main__':
+    for _ in range(int(input())):
+        n = int(input())
+        string = input()
+        ans = is_palindrome(string, n)
+        print(ans)
---
+++ linked-lists/nth-from-end.py
@@ -0,0 +1,13 @@
+"""
+https://practice.geeksforgeeks.org/problems/nth-node-from-end-of-linked-list/1
+"""
+
+"""
+Your task is to return the data stored in
+the nth end from end of linked list.
+"""
+"""
+can be done by traversal or recursion
+"""
+def getNthfromEnd(head,n):
+    #pass
