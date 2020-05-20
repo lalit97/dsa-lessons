a8538d6bfcd3966102829e43ccac479b855be25b
lalit97 <sutharlalit.97@gmail.com>
2020-05-20 09:19:11 +0530

    update

--- linked-lists/nth-from-end.py
+++ linked-lists/nth-from-end.py
@@ -7,7 +7,10 @@ Your task is to return the data stored in
 the nth end from end of linked list.
 """
 """
-can be done by traversal or recursion
+can be done by either of the 3 methods
+traversal 
+recursion
+2 pointers(not runner up technique) + simple maths 
 """
 def getNthfromEnd(head,n):
     #pass
