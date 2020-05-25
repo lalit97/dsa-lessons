f95b43b15d7b8774f4692a5184427178331cc55e
lalit97 <sutharlalit.97@gmail.com>
2020-05-25 20:06:23 +0530

    new problems

---
+++ linked-lists/day06-detect-and-remove-loop.py
@@ -0,0 +1,14 @@
+"""
+https://practice.geeksforgeeks.org/problems/remove-loop-in-linked-list/1
+"""
+
+"""
+Detect if there is a loop present in the linked list
+if present try to remove that.
+
+hint: Runner up technique
+""" 
+
+
+def removeTheLoop(head):
+    pass
---
+++ linked-lists/day06-occurence-of-an-int.py
@@ -0,0 +1,17 @@
+"""
+https://practice.geeksforgeeks.org/problems/occurence-of-an-integer-in-a-linked-list/1
+"""
+
+""" 
+Arguments: linked list head, k
+Return: total number of times k present in the linked list.
+
+example
+
+1 -> 2 -> 2 -> 3 -> 2 -> 5
+k = 2
+return 3 (2 appeares 3 times in list)
+"""
+
+def count(head, k):
+    pass
