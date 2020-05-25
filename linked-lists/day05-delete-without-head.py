9adc5c10650ffb0f5cfafe362b6db28771ef2618
lalit97 <sutharlalit.97@gmail.com>
2020-05-25 19:53:10 +0530

    add solution

--- linked-lists/day05-delete-without-head.py
+++ linked-lists/day05-delete-without-head.py
@@ -10,10 +10,26 @@ Function Arguments: node (given node to be deleted)
 Return Type: None, just delete the given node from the linked list.
 """
 
-1 -> 2 -> 3 -> 4 -> 5 
+1 -> 2 -> 4 -> 5 -> 6 -> 7 -> 8 -> 9 
 
-def deleteNode(curr_node):
-    pass
 
-# head
-# delete
+def deleteNode(curr):
+    if curr is None:
+        return None
+    while curr.next.next is not None:
+        curr.data = curr.next.data
+        curr = curr.next
+    curr.data = curr.next.data
+    curr.next = None
+
+"""
+alternate solution
+def deleteNode(curr):
+    if curr is None:
+        return None
+    next_node = curr.next
+    curr.data = next_node.data
+    to_delete = next_node
+    curr.next = next_node.next
+    to_delete.next = None
+"""
--- linked-lists/day05-intersection-pt.py
+++ linked-lists/day05-intersection-pt.py
@@ -1,6 +1,7 @@
 """
 https://practice.geeksforgeeks.org/problems/intersection-point-in-y-shapped-linked-lists/1
 """
+
 """
 Your task is to return the point of intersection
 in two linked list, connected in y shaped form.
@@ -10,7 +11,7 @@ Function Arguments: head_a, head_b (heads of both the lists)
 Return Type: NODE present at the point of intersection, or -1 if no common point.
 """
 
-1 -> 2 -> 3 -> 8 -> 9 -> 10 -> 12 -> 14 -> 16
+1 -> 2 -> 3 -> 8 -> 9 -> 10 -> 12
 
 4 -> 5 -> / 
 """
@@ -18,18 +19,34 @@ list1 -> before intersection point length = x
 list2 -> before intersection point length = y
 after intersection point length = z
 
-x + z + 
-y + z + 
+x + z + y
+y + z + x
 
-think about changing head
+3 + 4 + 2
+2 + 4 + 3
+
+####
+not intersecting
+1 -> 2 -> 3 -> 4
+
+5 -> 6 -> 7
+
+4 + 3 = 7
+3 + 4 = 7
 """
-while head_a != head_b:
-    if head_a is not None:
-        pass
-    else:
-        pass
-    if head_b is not None:
-        pass
-    else:
-        pass
-return -1
+
+def intersetPoint(head_a, head_b):
+    curr_a = head_a
+    curr_b = head_b
+    while curr_a != curr_b:
+        if curr_a is not None:
+            curr_a = curr_a.next
+        else:
+            curr_a = head_b
+        if curr_b is not None:
+            curr_b = curr_b.next
+        else:
+            curr_b = head_a
+    if curr_a is None:
+        return -1
+    return curr_a
