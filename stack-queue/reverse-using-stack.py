afa5621c8361385ab91d20b59a107a3f2b6b8420
lalit97 <sutharlalit.97@gmail.com>
2020-06-12 09:53:34 +0530

    new problem

---
+++ stack-queue/reverse-using-stack.py
@@ -0,0 +1,11 @@
+"""
+https://practice.geeksforgeeks.org/problems/reverse-a-string-using-stack/1
+"""
+
+
+def reverse(string):
+    """
+    argument: input string
+    return type: reversed string
+    """
+    my_stack = Stack()
