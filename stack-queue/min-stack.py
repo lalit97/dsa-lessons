910d4dbbba616a13eab678017d55b9e5aaa172d5
lalit97 <sutharlalit.97@gmail.com>
2020-06-13 00:08:55 +0530

    new problems

---
+++ stack-queue/min-stack.py
@@ -0,0 +1,18 @@
+"""
+https://practice.geeksforgeeks.org/problems/get-minimum-element-from-stack/1
+"""
+
+
+class stack:
+    def __init__(self):
+        self.s = []
+        self.minEle = None
+
+    def push(self, x):
+        # CODE HERE
+
+    def pop(self):
+        # CODE HERE
+
+    def getMin(self):
+        # CODE HERE
---
+++ stack-queue/sort-stack.py
@@ -0,0 +1,12 @@
+"""
+https://practice.geeksforgeeks.org/problems/sort-a-stack/1
+"""
+
+# your task is to complete this function
+# function sort the stack such that top element is max
+# funciton should return nothing
+# s is a stack
+
+
+def sorted(s):
+    return s.sort(reverse=True)
