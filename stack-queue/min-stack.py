dcd950ba9be3949bda26869846527a785e0472c7
lalit97 <sutharlalit.97@gmail.com>
2020-06-14 14:16:22 +0530

    update template

--- stack-queue/min-stack.py
+++ stack-queue/min-stack.py
@@ -50,26 +50,15 @@ class stack:
         """
         push item to stack
         """
-        if self.min_vals.is_empty() or x <= self.min_vals.peek():
-            self.min_vals.push(x)
-        self.stack1.push(x)
 
     def pop(self):
         """
         pop and return the element
         if stack is empty return -1
         """
-        if self.stack1.peek() == self.min_vals.peek():
-            self.min_vals.pop()
-        if self.stack1.top is not None:
-            return self.stack1.pop()
-        return -1
 
     def getMin(self):
         """
         return the minimum element
         if stack is empty return -1
         """
-        if self.min_vals.top is not None:
-            return self.min_vals.peek()
-        return -1
