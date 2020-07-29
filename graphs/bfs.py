21fe0a7fcaba08c5ba6302cf741596688e5bbeaf
lalit97 <sutharlalit.97@gmail.com>
2020-07-29 14:23:55 +0530

    graph traversals

---
+++ graphs/bfs.py
@@ -0,0 +1,17 @@
+graph = {
+  'A' : ['B','C'],
+  'B' : ['D', 'E'],
+  'C' : ['F'],
+  'D' : [],
+  'E' : ['F'],
+  'F' : []
+}
+
+visited = [] # List to keep track of visited nodes.
+queue = []     #Initialize a queue
+
+def bfs(visited, graph, node):
+    pass 
+
+# Driver Code
+bfs(visited, graph, 'A')
---
+++ graphs/dfs.py
@@ -0,0 +1,17 @@
+# Using a Python dictionary to act as an adjacency list
+graph = {
+    'A' : ['B','C'],
+    'B' : ['D', 'E'],
+    'C' : ['F'],
+    'D' : [],
+    'E' : ['F'],
+    'F' : []
+}
+
+visited = set() # Set to keep track of visited nodes.
+
+def dfs(visited, graph, node):
+    pass
+
+# Driver Code
+dfs(visited, graph, 'A')
